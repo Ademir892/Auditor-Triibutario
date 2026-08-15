825522693c6907688dee412434d39a2623b3c00e
Ademir Marmitt Traesel <ademirtraesel@hotmail.com>
2026-08-14 23:06:44 -0300

    chore: initialize project structure

---
+++ backend/src/main/java/br/com/auditortributario/AuditorTributarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.auditortributario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditorTributarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditorTributarioApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/auditortributario/AuditorTributarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.auditortributario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditorTributarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
